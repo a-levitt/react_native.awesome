b255c4649f24d05a4ebb7a35745a921a66fb7e08
Artem Trifonov <artemis.levitt@gmail.com>
2025-02-23 15:50:30 +0100

    Adjust borders

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 import SafeViewAndroid from './components/SafeViewAndroid';
 
 function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
+      <View style={{
+        borderWidth: 3,
+        borderTopWidth: 5,
+        borderBottomWidth: 10,
+        borderRightWidth: 20,
+        borderColor: '#15099d',
+        borderRadius: 10,
+        borderBottomLeftRadius: 15,
+      }}>
       <Text>Hello</Text>
+      </View>
     </SafeAreaView>
   );
 }
