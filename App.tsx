8fee6e0fc02a7712c742ee349d0c7d6349abf953
Artem Trifonov <artemis.levitt@gmail.com>
2025-02-23 16:08:30 +0100

    Adjust margins

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ function App(): React.JSX.Element {
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
       <View style={{
+        /*borders*/
         borderWidth: 3,
         borderTopWidth: 5,
         borderBottomWidth: 10,
@@ -14,8 +15,22 @@ function App(): React.JSX.Element {
         borderColor: '#15099d',
         borderRadius: 10,
         borderBottomLeftRadius: 15,
+        /*background color*/
+        backgroundColor: '#F0F8FF',
+        /*margins*/
+        margin: 2,
+        marginTop: 5,
+        marginLeft: 100,
+        marginBottom: 25,
+        // marginHorizontal: 100,
       }}>
       <Text>Hello</Text>
+      </View>
+      <View style={{
+        /*margins*/
+
+      }}>
+        <Text>Some text here</Text>
       </View>
     </SafeAreaView>
   );
