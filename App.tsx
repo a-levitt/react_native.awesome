112c37e52869c6a68336e34cdde12ec09211d81c
Artem Trifonov <artemis.levitt@gmail.com>
2025-02-26 15:03:20 +0100

    Use View components

--- App.tsx
+++ App.tsx
@@ -7,37 +7,50 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-      <View style={{
-        /*borders*/
-        borderWidth: 3,
-        borderTopWidth: 5,
-        borderBottomWidth: 10,
-        borderRightWidth: 20,
-        borderColor: '#15099d',
-        borderRadius: 10,
-        borderBottomLeftRadius: 15,
-        /*background color*/
-        backgroundColor: '#F0F8FF',
-        /*margins*/
-        margin: 2,
-        marginTop: 5,
-        marginLeft: 100,
-        marginBottom: 25,
-        // marginHorizontal: 100,
-        /*paddings*/
-        padding: 7,
-        paddingBottom: 5,
-        // paddingHorizontal: 100,
-      }}>
-      <Text>Hello</Text>
+      <View style={{backgroundColor: "#0039a6", alignItems: "center"}}>
+        <Text>
+          HEADER
+        </Text>
       </View>
-      <View style={{
-        /*positioning*/
-        position: 'relative', // default
-        // top: 10,
-        zIndex: 1, // more = closer to viewer
-      }}>
-        <MyText />
+      <View>
+        <Text>AwesomeApp (body)</Text>
+        <View style={{
+          /*borders*/
+          borderWidth: 3,
+          borderTopWidth: 5,
+          borderBottomWidth: 10,
+          borderRightWidth: 20,
+          borderColor: '#15099d',
+          borderRadius: 10,
+          borderBottomLeftRadius: 15,
+          /*background color*/
+          backgroundColor: '#F0F8FF',
+          /*margins*/
+          margin: 2,
+          marginTop: 5,
+          marginLeft: 100,
+          marginBottom: 25,
+          // marginHorizontal: 100,
+          /*paddings*/
+          padding: 7,
+          paddingBottom: 5,
+          // paddingHorizontal: 100,
+        }}>
+          <Text>Hello</Text>
+        </View>
+        <View style={{
+          /*positioning*/
+          position: 'relative', // default
+          // top: 10,
+          zIndex: 1, // more = closer to viewer
+        }}>
+          <MyText />
+        </View>
+      </View>
+      <View style={{alignItems: 'center', marginTop: 600}}>
+        <Text style={{color:'#B0C4DE'}}>
+          FOOTER
+        </Text>
       </View>
     </SafeAreaView>
   );
